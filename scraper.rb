75119f6c357c2f8d62c9d6178cef482ec4513d13
Tony Bowden <tony@tmtm.com>
2016-03-25 12:25:21 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -6,8 +6,7 @@ require 'wikidata/fetcher'
 
 # current = WikiData::Category.new( 'Flokkur:Alþingismenn', 'is').member_titles
 # former = WikiData::Category.new( 'Flokkur:Fyrrum Alþingismenn', 'is').member_titles
+# names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
-EveryPolitician::Wikidata.scrape_wikidata(names: { is: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
-
+names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wp', column: 'wikipedia')
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
