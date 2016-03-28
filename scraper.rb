1d79056b53cfbb6c5db1ec95b36143042fe04126
Tony Bowden <tony@tmtm.com>
2016-03-28 17:31:32 +0100

    Also get Icelandic names

--- scraper.rb
+++ scraper.rb
@@ -1,12 +1,12 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
 
-# current = WikiData::Category.new( 'Flokkur:Alþingismenn', 'is').member_titles
-# former = WikiData::Category.new( 'Flokkur:Fyrrum Alþingismenn', 'is').member_titles
-# names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
+current = WikiData::Category.new( 'Flokkur:Alþingismenn', 'is').member_titles.reject { |n| n.include? 'Kjörnir alþingismenn' }
+former = WikiData::Category.new( 'Flokkur:Fyrrum Alþingismenn', 'is').member_titles
+is_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
 
-names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wp', column: 'wikipedia')
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
+en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wp', column: 'wikipedia')
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, is: is_names | current | former }, output: false)
