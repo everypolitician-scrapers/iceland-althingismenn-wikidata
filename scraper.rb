367378036e7c13de2ebdc3df6d294336cfb4e24e
Tony Bowden <tony@tmtm.com>
2018-10-11 14:52:40 +0100

    Also find via Wikidata P39s

--- scraper.rb
+++ scraper.rb
@@ -5,8 +5,11 @@ require 'wikidata/fetcher'
 
 current = WikiData::Category.new( 'Flokkur:Alþingismenn', 'is').member_titles.reject { |n| n.include? 'Kjörnir alþingismenn' }
 former = WikiData::Category.new( 'Flokkur:Fyrrum Alþingismenn', 'is').member_titles
-is_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
 
+is_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wikipedia', column: 'wikiname')
 en_names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/iceland-althing-wp', column: 'wikipedia')
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en_names, is: is_names | current | former }, output: false)
+sparq = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39/wdt:P279* wd:Q21272959 }'
+p39s = EveryPolitician::Wikidata.sparql(sparq)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: en_names, is: is_names | current | former })
